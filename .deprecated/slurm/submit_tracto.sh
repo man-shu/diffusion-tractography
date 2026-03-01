cf963aa68ded3da4298b32cb760879281725b509
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2026-03-01 18:18:42 +0100

    add slurm

---
+++ .deprecated/slurm/submit_tracto.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name=tracto
+#SBATCH --partition=gpu-best,parietal
+#SBATCH --error error_%A_%a.out
+#SBATCH --gres=gpu:1
+
+singularity exec \
+--env-file /data/parietal/store3/work/haggarwa/diffusion/diffusion-tractography/singularity_env.txt \
+--bind /data/parietal/store3/work/haggarwa/diffusion/diffusion-tractography/data:/home/input \
+/data/parietal/store3/work/haggarwa/diffusion/diffusion-tractography/diffusion-tractography_main_singularity.sif \
+/opt/miniconda3/bin/tractography \
+/home/input/WAND-downsampled \
+/home/input/WAND-downsampled/derivatives \
+--work-dir /home/input/cache \
+--participant-label sub-00395 \
+--bids-filter-file /home/input/bids_filter.json \
+--roi-dir /home/input/rois-downsampled \
+--debug
