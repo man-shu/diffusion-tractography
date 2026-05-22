bb5911531e094567dfee5930eb9023e09988ae2d
Himanshu <himanshuaggarwal1997@gmail.com>
2026-05-22 12:43:54 +0200

    slurm script

--- .deprecated/slurm/submit_tracto.sh
+++ .deprecated/slurm/submit_tracto.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 #
-#SBATCH --job-name=tracto
-#SBATCH --partition=gpu-best,parietal
-#SBATCH --error error_%A_%a.out
-#SBATCH --gres=gpu:1
+#SBATCH --job-name=diffusion_pipelines
+#SBATCH --output=log_slurm/jobid_%A_%a.out 
+#SBATCH --error=log_slurm/jobid_%A_%a.err
+#SBATCH --partition=normal,parietal
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --time=48:00:00
+#SBATCH --array=0-95
 
-module load singularity
+dirs=(/data/parietal/store3/work/haggarwa/diffusion/data/stanford-bids/sub-*)
+echo ${dirs[${SLURM_ARRAY_TASK_ID}]:69}
 
-module load cuda
-
-singularity exec --nv \
+srun singularity exec \
 --env-file /data/parietal/store3/work/haggarwa/diffusion/diffusion-tractography/singularity_env.txt \
 --bind /data/parietal/store3/work/haggarwa/diffusion/diffusion-tractography/data:/home/input \
 /data/parietal/store3/work/haggarwa/diffusion/diffusion-tractography/diffusion-tractography_main_singularity.sif \
 /opt/miniconda3/bin/tractography \
-/home/input/WAND-downsampled \
-/home/input/WAND-downsampled/derivatives \
+/home/input/stanford-bids \
+/home/input/stanford-bids/derivatives \
 --work-dir /home/input/cache \
---participant-label sub-00395 \
---bids-filter-file /home/input/bids_filter.json \
---roi-dir /home/input/rois-downsampled \
+--participant-label ${dirs[${SLURM_ARRAY_TASK_ID}]:69} \
+--session-label ses-01 \
+--roi-dir /home/input/rois \
+--bids-filter /home/input/bids_filter.json \
 --debug
